07c315e6afbec2c68ee64b0c71f9dbc095b7d921
romistaro <romit.bali@gmail.com>
2023-07-25 09:46:21 -0400

    Added textAnalyzer Class

---
+++ textAnalyzer.py
@@ -0,0 +1,31 @@
+class textanalysis(object):
+
+	def __init__(self, text):
+		
+		formattedtext = text.replace("!"," ").replace("?"," ").replace("."," ").replace(",", " ")
+		
+		formattedtext.lower()
+	
+		self.fmttext = formattedtext
+		
+	def countAll(self):
+
+		textset = self.fmttext.split(" ")
+		
+		textdict = {}
+		
+		for i in textset:
+			textdict[i] = textset.count(i)
+
+		return textdict
+
+	def freqOf(self, word):
+		
+		textdict = self.countAll
+
+		if (word in textdict):
+			return textdict[word]
+		else:
+			return 0
+
+
